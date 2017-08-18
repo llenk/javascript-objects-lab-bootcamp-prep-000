ec24a72d433183d6fc9068f3bd3b75d22a408909
Ellen Keal <ellenkeal@gmail.com>
2017-08-18 02:13:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = new Objects({flour: "8 cups"};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, {key: value});
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
