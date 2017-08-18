d61e94f913392e3fff1f0d5bc6837e7dade5482e
Ellen Keal <ellenkeal@gmail.com>
2017-08-18 02:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object({flour: "8 cups"});
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign({}, object, {key: value});
+  var newObj = Object.assign({}, object, {[key]: value});
   return newObj;
 }
 
